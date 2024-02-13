9cfecbe264172345b86926f417a4fd9d784dc71c
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2024-02-13 21:14:21 +0900

    [plot by two yaml file] first plot of two bag data

--- rosbag_plot.py
+++ rosbag_plot.py
@@ -6,8 +6,9 @@ import sys
 import os
 import matplotlib.pyplot as plt
 import yaml
+from typing import List, Dict
 
-class bagPlotter:
+class bagLoader:
     def __init__(self, obj):
         self.bag_filename = os.path.join(os.getcwd(), obj["bag_filename"])
         self.bag = rosbag.Bag(self.bag_filename)
@@ -22,8 +23,8 @@ class bagPlotter:
         self.end_time = obj["end_time"]
         self.show_legend = obj["show_legend"]
         self.legends = []
-        if(self.show_legend):
-            self.legends = obj["legends"]
+#         if(self.show_legend):
+        self.legends = obj["legends"]
         self.x_label = obj["x_label"]
         self.y_label = obj["y_label"]
         self.y_range = obj["y_range"]
@@ -60,33 +61,65 @@ class bagPlotter:
                         self.datas[j] = np.append(self.datas[j], data)
                         self.times[j] = np.append(self.times[j], rosbag_time - self.start_time)
 
-        # plot data
-        plt.rcParams['font.family'] = 'Times New Roman' # https://kenbo.hatenablog.com/entry/2018/11/28/111639
-        plt.rcParams['figure.subplot.bottom'] = 0.2     # https://qiita.com/78910jqk/items/e8bce993081c384afdba
-        fig = plt.figure(figsize=obj["fig_size"])
-        for i in range(len(self.plot_topic_names)):
-            plt.plot(self.times[i], self.datas[i], color=self.plot_colors[i], label=self.legends[i])
-        if(self.show_legend):
-            plt.legend(frameon=False, fontsize=obj["legend_fontsize"])
-        plt.xlabel(self.x_label, fontsize=obj["x_label_fontsize"])
-        plt.ylabel(self.y_label, fontsize=obj["y_label_fontsize"])
-        plt.ylim(self.y_range)
-        plt.tick_params(labelsize=obj["label_fontsize"])
+class bagPlotter:
+    def __init__(self, bag_data_list: List[Dict[bagLoader, Dict]]):
+        obj = bag_data_list[0]["obj"]
+
+        self.fig = plt.figure(figsize=obj["fig_size"])
+        self.fig.suptitle("several_plot_check")
+        
+        for i, bag_yaml_data in enumerate(bag_data_list):
+            print("type(bag_yaml_data): ", type(bag_yaml_data))
+            print("bag_yaml_data.keys(): ",bag_yaml_data.keys())
+            data_dim:int = np.ceil(np.sqrt(len(bag_data_list)))
+            self.plot_bag_data(length = data_dim, number = i, **bag_yaml_data)
+
         if(obj["save_fig"]):
             plt.savefig(obj["save_fig_name"])
         else:
             plt.show()
 
+    def plot_bag_data(self, length: int, number: int, bag_data: bagLoader, obj: Dict): #ax is type of np.array, which includes matplotlib.axes
+        # plot data
+        plt.rcParams['font.family'] = 'Times New Roman' # https://kenbo.hatenablog.com/entry/2018/11/28/111639
+        plt.rcParams['figure.subplot.bottom'] = 0.2     # https://qiita.com/78910jqk/items/e8bce993081c384afdba
+        print("length of plot: ", len(bag_data.plot_topic_names))
+        print("length of times: ", len(bag_data.times))
+        print("length of datas: ", len(bag_data.datas))
+        print("length of colors: ", len(bag_data.plot_colors))
+        print("length of legends: ", len(bag_data.legends))
+
+        print("length: ",length)
+        print("number: ",number)
+        ax = self.fig.add_subplot(length,length,number + 1)
+
+        for i in range(len(bag_data.plot_topic_names)):
+            ax.plot(bag_data.times[i], bag_data.datas[i], color=bag_data.plot_colors[i], label=bag_data.legends[i])
+        if(bag_data.show_legend):
+            ax.legend(frameon=False, fontsize=obj["legend_fontsize"])
+        ax.set_xlabel(bag_data.x_label, fontsize=obj["x_label_fontsize"])
+        ax.set_ylabel(bag_data.y_label, fontsize=obj["y_label_fontsize"])
+        ax.set_ylim(bag_data.y_range)
+        ax.tick_params(labelsize=obj["label_fontsize"])
+
 
 if __name__ == "__main__":
-    config_path = ""
-    if(len(sys.argv) == 2):
-        config_path = sys.argv[1]
+    length_argv = len(sys.argv)
+    config_path_list = [""]*(length_argv-1)
+    if(length_argv >= 2):
+        for i in range(length_argv-1):
+            config_path_list[i] = sys.argv[i+1]
+        print("config_path_list: ", config_path_list)
+
     else:
         print("Please specify the config file name!")
         sys.exit()
 
-    with open(config_path) as file:
-        obj = yaml.safe_load(file)
+    bag_data_list = []
+    for config_path in config_path_list:
+         with open(config_path) as file:
+            obj: Dict = yaml.safe_load(file)
+            bag_data = bagLoader(obj)
+            bag_data_list.append({"bag_data": bag_data, "obj": obj})
 
-        bagPlotter(obj)
+    bagPlotter(bag_data_list)
